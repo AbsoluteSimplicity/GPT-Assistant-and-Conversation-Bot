c4b0851d5b2f525e4cac3aa386aed4adbbb6f366
TotalSimplicity <68793354+TotalSimplicity@users.noreply.github.com>
2023-02-28 08:20:31 -0500

    Random things

--- audiooutput.py
+++ audiooutput.py
@@ -1,15 +1,12 @@
 import os
-import boto3
+import boto3 as b3
 import config as cnf
 
 os.environ['AWS_ACCESS_KEY_ID'] = cnf.aws_access_key_id
 os.environ['AWS_SECRET_ACCESS_KEY'] = cnf.aws_secret_key
 voice_id = 'Arthur'
 # Create a client for the Amazon Polly service
-polly = boto3.client('polly', region_name='us-east-1')
-
-# Set the input text and voice
-text = 'Hello, world!'
+polly = b3.client('polly', region_name='us-east-1')
 
 def speak_response(response):
     # Generate the speech using Amazon Polly
